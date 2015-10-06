178c492f96a720233512ca682992e88221125210
Cristian Sulea <cristian.sulea.79@gmail.com>
2015-10-06 13:00:26 +0300

    package-info.java

---
+++ jatoo-properties/src/main/java/jatoo/properties/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * A Java™ Open Source library created to ease the work with properties.
+ * 
+ * @author <a href="http://cristian.sulea.net" rel="author">Cristian Sulea</a>
+ */
+package jatoo.properties;
+
